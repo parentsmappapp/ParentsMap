809658802102c6925c0784ea9156b08c392fdabc
Mariia <myepimakhina@mail.ru>
2026-05-30 20:16:29 +1000

    Initial Commit

---
+++ ParentsMap/ParentsMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParentsMapApp.swift
+//  ParentsMap
+//
+//  Created by Mariia on 30/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct ParentsMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParentsMapTests/ParentsMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParentsMapTests.swift
+//  ParentsMapTests
+//
+//  Created by Mariia on 30/5/2026.
+//
+
+import Testing
+@testable import ParentsMap
+
+struct ParentsMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
